e80b926e4e9af1af1c5e4ab67efbe39da3eec500
seungyeon <seung43422@gmail.com>
2025-08-12 00:51:23 +0900

    [feat] 아토믹 코틀린 1주차 과제_7/8

--- src/main/kotlin/week01/ccc00s1.kt
+++ src/main/kotlin/week01/ccc00s1.kt
@@ -2,4 +2,56 @@ package week01
 
 fun main() {
 
+    val input = System.`in`.bufferedReader()
+    val output = System.out.bufferedWriter()
+
+    var quarter = input.readLine().toInt()
+    var machine1 = input.readLine().toInt()
+    var machine2 = input.readLine().toInt()
+    var machine3 = input.readLine().toInt()
+    var playCount = 0 // 총 플레이 횟수
+
+    if (quarter !in 1..1000) {
+        return
+    }
+
+    while(quarter > 0) {
+        quarter--
+        playCount++
+        machine1++
+        if(machine1 == 35){
+            quarter += 30
+            machine1 = 0 // 30쿼터 지급 후 초기화
+        }
+
+        if(quarter == 0){
+            break
+        }
+
+        quarter--
+        playCount++
+        machine2++
+        if(machine2 == 100){
+            quarter += 60
+            machine2 = 0 // 60쿼터 지급 후 초기화
+        }
+        if(quarter == 0){
+            break
+        }
+
+        quarter--
+        playCount++
+        machine3++
+        if(machine3 == 10){
+            quarter += 9
+            machine3 = 0
+        }
+        if(quarter == 0){
+            break
+        }
+    }
+    output.write("Martha plays $playCount times before going broke.")
+    output.flush()
+
+
 }
---
+++ src/main/kotlin/week01/ccc08j2.kt
@@ -0,0 +1,8 @@
+package week01
+
+fun main() {
+    val input = System.`in`.bufferedReader()
+    val output = System.out.bufferedWriter()
+
+    //(b:누를 번호,n: 누를 횟수) 마지막은 무조건 (4,1)
+}
