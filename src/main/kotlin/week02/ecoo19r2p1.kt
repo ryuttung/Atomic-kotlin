982ab79f96bb6d4d8b0c44fee3f4e5a97445ed66
seungyeon <seung43422@gmail.com>
2025-09-09 00:59:36 +0900

    [feat] 아토믹 코틀린 2주차: 문제 해결 4/8 (ecoo19r2p1)

--- src/main/kotlin/week02/ecoo19r2p1.kt
+++ src/main/kotlin/week02/ecoo19r2p1.kt
@@ -2,25 +2,35 @@ package week02
 
 fun main() {
     val input = System.`in`.bufferedReader()
-    val output = System.out.bufferedWriter()
 
-    repeat(10) {
-        val N = input.readLine().toInt()
+    val datasets = 10
+    var i = 0
+
+    while (i < datasets) {
+        val N = if (i < 6) {
+            inputInt(input.readLine(), 1..100)
+        } else {
+            inputInt(input.readLine(), 1..100_000)
+        }
+
         val emailSet = mutableSetOf<String>()
 
-        val i = 0
-        repeat(N) {
-            val email = input.readLine().trim().lowercase()
+        var j = 0
+        while (j < N) {
+            val email = input.readLine()
+            if (email.length !in 1..30) throw Throwable("Invalid email length")
 
-            val idPart = email.substringBefore('@')
-            val domain = email.substringAfter('@')
+            val idPart = email.trim().lowercase().substringBefore('@')
+            val domain = email.trim().lowercase().substringAfter('@')
 
             val id = idPart.substringBefore('+').replace(".", "")
 
             val realEmail = "$id@$domain"
             emailSet.add(realEmail)
+            j++
         }
-        println(emailSet.size)
-    }
 
+        println(emailSet.size)
+        i++
+    }
 }
