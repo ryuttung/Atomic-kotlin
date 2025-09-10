2b65605709f75e815c6bb0f3a55882d98b8a5907
seungyeon <seung43422@gmail.com>
2025-09-11 00:10:12 +0900

    [feat] 아토믹 코틀린 2주차: 문제 해결 6/8 (coci17c2p2,crci06p1)

--- src/main/kotlin/week02/coci17c2p2.kt
+++ src/main/kotlin/week02/coci17c2p2.kt
@@ -1,6 +1,61 @@
 package week02
 
 fun main() {
+
     val input = System.`in`.bufferedReader()
-    val output = System.out.bufferedWriter()
+
+    val split = input.readLine().split(" ")
+    val K = inputInt(split[0], 1..100_000) //단어 개수
+    val N = inputInt(split[1], 1..100_000) //Zig가 말하는 횟수
+
+    val dictMap = mutableMapOf<Char, MutableList<String>>() //단어 목록
+    val countMap = mutableMapOf<String, Int>()
+
+    var i = 0
+    while (i < K) {
+        val word = input.readLine()
+        if (word.length > 21) throw Throwable("word length over 21")
+
+        val firstChar = word[0] //첫 글자만 따서 맵핑
+        if (dictMap.containsKey(firstChar)){
+            dictMap[firstChar]!!.add(word) //맵은 무조건 null 아니라는 거 해줘야함 !!
+        }else {
+            dictMap[firstChar] = mutableListOf(word) //첫글자 없으면 리스트 새로 만들어줘야 함
+        }
+
+        countMap[word] = 0 //초기화
+        i++
+    }
+
+    for (key in dictMap.keys){
+        //사전 오름차순
+        dictMap[key]!!.sort()
+    }
+
+    var j = 0
+    while (j < N) {
+        val zig = input.readLine()[0] //Zig가 말하는 첫 글자
+
+        val enableWords = dictMap[zig] ?: throw Throwable("No words found for zig: $zig")
+
+        // 1. 적게 불린 단어
+        // 2. 그 수가 같다면 사전순으로 앞서는 단어
+        // 단어들을 순회하면서 그 단어가 기준 단어보다 덜 불렸다면 그 단어를 기준 단어로 교체
+
+        var zag = enableWords[0]
+        var zagCount = countMap[zag] ?: throw Throwable("No count found for zag: $zag")
+
+        for (i in 1 until enableWords.size) {
+            val word = enableWords[i]
+            val wordCount = countMap[word] ?: throw Throwable("No count found for word: $word")
+        if (wordCount < zagCount) {
+            zag = word
+            zagCount = wordCount
+        }
+    }
+        countMap[zag] = countMap[zag]!! + 1
+        println(zag)
+        j++
+    }
+
 }
--- src/main/kotlin/week02/crci06p1.kt
+++ src/main/kotlin/week02/crci06p1.kt
@@ -3,24 +3,53 @@ package week02
 fun main() {
     val input = System.`in`.bufferedReader()
 
-    //bard(1)번이 오면 새로운 노래를 가지고 온다
-    //앞은 참여 인원 수, 뒤는 참여한 인원 번호
+    val bard = 1
 
-    val N = input.readLine().toInt()
-    if(N !in 2..100) throw Throwable("out of range 2..100 F: $N")
+    val N = inputInt(input.readLine(), 2..100) //인원 수
+    val E = inputInt(input.readLine(), 1..50) //일 수
 
-    val E = input.readLine().toInt()
-    if(E !in 1..50) throw Throwable("out of range 1..50 E: $E")
+    val knows = mutableListOf<MutableSet<Int>>() //인덱스 번호 = 주민 번호, set = 주민이 알고 있는 노래들
+    repeat(N+1){
+        knows.add(mutableSetOf())
+    }
 
+    var songSize = 0
     var i = 0
-    while(i < E) {
-        val line = input.readLine().split(" ")
-        val K = line[0].toInt()
-        if(K !in 2..N) throw Throwable("out of range 2..$N K: $K")
+    while (i < E){
+        val split = input.readLine().split(" ")
+        val K = inputInt(split[0], 2..N)
 
+        //인덱스 번호 = 주민 번호
+        val peoples = mutableListOf<Int>()
+        for(j in 1..K){
+            val p = inputInt(split[j], 1..N)
+            peoples.add(p)
+        }
 
+        if(bard in peoples){
+            songSize++
+            for(p in peoples){
+                knows[p].add(songSize)
+            }
+        } else {
+            val allSongs = mutableSetOf<Int>() //모든 사람들이 알고있는 노래를 담는 곳
 
+            for(p in peoples){
+                allSongs.addAll(knows[p])
+            }
+
+            //모든 노래 공유
+            for(p in peoples){
+                knows[p].clear()
+                knows[p].addAll(allSongs)
+            }
+        }
         i++
+    }
+    for(v in 1..N){
+        if(knows[v].size == songSize){
+            println(v)
+        }
     }
 
 }
